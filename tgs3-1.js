0cc48fa806fcec741e6ea99ae02e8c9ec87c93f5
santiaisabela20 <117247746+santiaisabela20@users.noreply.github.com>
2023-10-17 20:48:25 +0700

    Add files via upload

---
+++ tgs3-1.js
@@ -0,0 +1,9 @@
+const restaurant = {
+    name: "bakso titoti",
+    city: "tanggerang",
+    "favorite drink": "lemontea",
+    "favorite food": "mie ayam bakso",
+    isVegan: true,
+};
+
+console.table(restaurant)
